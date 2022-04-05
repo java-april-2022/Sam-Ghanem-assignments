e77e9b421a83cff67e697a8ad406341d895e29ac
SamGhanem <yambo78@outlook.com>
2022-04-05 14:19:44 -0400

    commit

---
+++ Week1/Extra Practice/First_Java.java
@@ -0,0 +1,27 @@
+public class First_Java{
+    public static void main(String[] args) {
+        name("Sam Ghanem");
+        age(30);
+        home("Great Falls");
+    }
+    private static void name(String name) {
+        System.out.println("My name is " + name);
+    }
+    
+    private static void age(int age) {
+        System.out.println("I am " + age + " years old");
+    }
+
+    private static void home(String home) {
+        System.out.println("I am from " + home);
+    }
+
+
+
+    {
+    // public static void name(String name) {
+    //     System.out.println("My name is " + name);
+    // }
+    }
+}
+
